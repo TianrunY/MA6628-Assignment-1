3563462732d885211c82e0bf249480dddf7559d8
TianrunY <43376880+TianrunY@users.noreply.github.com>
2018-10-03 15:45:04 +0800

    New change for assignment 1

--- 2018-10-03-125913.py
+++ 2018-10-03-125913.py
@@ -1,6 +1,13 @@
+# MA 6628 Assignment 1
+# Tianrun Yu
+
 import math
 import sys
-def isprime(n):
+def isprime (n):
+    if n <= 1:
+        return 0
+    if n%2 == 0:
+        return 0
     for x in range(3,int(n**0.5)+1,2):
         if n%x == 0:
             return 0
@@ -12,5 +19,5 @@ if __name__ == "__main__":
         if isprime(x)*isprime(x+2):
             a,b=x,x+2
             count = count +1
-    print('The total number of twin primes between 1000 and 1000000 is %d' %count)
+    print('The total number of twin primes between one thousand and one million is %d' %count)
     print('The biggest twin prime is %d %d' %(a,b))
